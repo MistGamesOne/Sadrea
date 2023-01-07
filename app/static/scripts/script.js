c67e6348f63e42bcaa4ac94fb6411ac87dc11120
Matvey <matvey@a.ru>
2023-01-07 19:02:49 +0300

    изменения сайта

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,6 +1,9 @@
 const header = document.getElementById("header");
 const menu = document.getElementById("menu");
 const companyInfo = document.getElementById("company-info");
+const tournamentsInfo = document.getElementById("tournaments-info");
+const tolkingInfo = document.getElementById("tolking-info");
+const lecensyInfo = document.getElementById("lecensy-info");
 
 document.getElementById("open-company-info").addEventListener("click", () => {
     header.style.display = "none";
@@ -12,4 +15,47 @@ document.getElementById("close-company-info").addEventListener("click", () => {
     header.style.display = "block";
     menu.style.display = "block";
     companyInfo.style.display = "none";
+});
+document.getElementById("open-tournaments-info").addEventListener("click", () => {
+    header.style.display = "none";
+    menu.style.display = "none";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "block";
+});
+document.getElementById("close-tournaments-info").addEventListener("click", () => {
+    header.style.display = "block";
+    menu.style.display = "block";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "none";
+});
+document.getElementById("open-tolking-info").addEventListener("click", () => {
+    header.style.display = "none";
+    menu.style.display = "none";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "none";
+    tolkingInfo.style.display = "block";
+});
+
+document.getElementById("close-tolking-info").addEventListener("click", () => {
+    header.style.display = "block";
+    menu.style.display = "block";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "none";
+    tolkingInfo.style.display = "none";
+});
+document.getElementById("open-lecensy-info").addEventListener("click", () => {
+    header.style.display = "none";
+    menu.style.display = "none";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "none";
+    tolkingInfo.style.display = "none";
+    lecensyInfo.style.display = "block";
+});
+document.getElementById("close-lecensy-info").addEventListener("click", () => {
+    header.style.display = "block";
+    menu.style.display = "block";
+    companyInfo.style.display = "none";
+    tournamentsInfo.style.display = "none";
+    tolkingInfo.style.display = "none";
+    lecensyInfo.style.display = "none";
 });
